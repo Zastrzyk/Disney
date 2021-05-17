2776a266bea16d1c77ecf2f165c95364757815d6
Zastrzyk <r.ogrodowczyk87@gmail.com>
2021-05-17 11:18:39 +0100

    header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Header / >
+        <
+        Home / >
+        <
+        /div>
+    );
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,61 +2,82 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Header = () => {
-    return (
-        <Nav>
-            <Logo src="/images/logo.svg"/>
-            <NavMenu>
-                <a>
-                    <img src="/images/home-icon.svg"/>
-                    <span>HOME</span>
-                </a>
+    return ( <
+        Nav >
+        <
+        Logo src = "/images/logo.svg" / >
+        <
+        NavMenu >
+        <
+        a >
+        <
+        img src = "/images/home-icon.svg" / >
+        <
+        span > HOME < /span> <
+        /a>
 
-                <a>
-                    <img src="/images/search-icon.svg"/>
-                    <span>SEARCH</span>
-                </a>
+        <
+        a >
+        <
+        img src = "/images/search-icon.svg" / >
+        <
+        span > SEARCH < /span> <
+        /a>
 
-                <a>
-                    <img src="/images/watchlist-icon.svg"/>
-                    <span>WATCHLIST</span>
-                </a>
+        <
+        a >
+        <
+        img src = "/images/watchlist-icon.svg" / >
+        <
+        span > WATCHLIST < /span> <
+        /a>
 
-                <a>
-                    <img src="/images/original-icon.svg"/>
-                    <span>ORIGINAL</span>
-                </a>
+        <
+        a >
+        <
+        img src = "/images/original-icon.svg" / >
+        <
+        span > ORIGINAL < /span> <
+        /a>
 
-                <a>
-                    <img src="/images/movie-icon.svg"/>
-                    <span>MOVIES</span>
-                </a>
-            </NavMenu>
-            <UserImg src="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"/>
-        </Nav>
-      
+        <
+        a >
+        <
+        img src = "/images/movie-icon.svg" / >
+        <
+        span > MOVIES < /span> <
+        /a> <
+        /NavMenu> <
+        UserImg src = "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png" / >
+        <
+        /Nav>
+
     )
 }
 
 export default Header
 
-const Nav = styled.nav`
+const Nav = styled.nav `
     height: 70px;
     background: #090b13;
     display: flex;
     align-items: center;
     padding: 0 36px;
 `
-const Logo = styled.img`
+const Logo = styled.img `
     width: 80px;
 `
-const NavMenu = styled.div`
+const NavMenu = styled.div `
     display: flex;
     flex: 1;
+    margin-flex: 25px;
+    align-items: center;
 
     a {
         display: flex;
         align-items: center;
         padding: 0 12px;
+        cursor: pointer;
 
         img {
             height: 20px;
@@ -65,12 +86,17 @@ const NavMenu = styled.div`
         span {
             font-size: 13px;
             letter-spacing: 1.42px;
+
+            &:after {
+                content: "";
+                height: 2px;
+            }
         }
     }
 
 `
 
-const UserImg = styled.img`
+const UserImg = styled.img `
     width: 48px;
     height: 48px;
     cursor: pointer;
