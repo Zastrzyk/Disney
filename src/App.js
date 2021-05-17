08d0150f6bf30ee03031b5575ac611360528258c
Zastrzyk <r.ogrodowczyk87@gmail.com>
2021-05-17 13:00:48 +0100

    ImgSlider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header';
-import Home from './Components/Home';
+import Home from './Components/Home'
+import './App.css';
 
 function App() {
   return (
