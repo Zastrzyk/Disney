ce68f81aa3d773c2fbf1fb99ec1a162f718bdb54
Zastrzyk <r.ogrodowczyk87@gmail.com>
2021-05-30 12:37:20 +0100

    Login.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './Components/Header';
 import Home from './Components/Home'
 import './App.css';
 import Detail from './Components/Detail';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import Login from './Components/Login';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Detail />
           </Route>
---
+++ src/Components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Login = () => {
+    return (
+        <Container>
+            Login
+        </Container>
+    )
+}
+
+export default Login
+
+
+const Container = styled.div`
+    position: relative;   
+    height: calc(100vh - 70px);
+
+    &:before {
+        background-position: top;
+        background-size: cover;
+        position: absolute;
+        content: "";
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        background-image: url("/images/login-background.jpg");
+        z-index: -1;
+    }
+`
